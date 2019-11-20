da6c71a43d6a0363d0f7ad6cf3b5636c6908db0e
Fat-Tony <fat_tonyy@icloud.com>
2019-11-20 18:42:46 +0000

    landingpage done

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
+
+const Home = () => {
+  return (
+    <>
+      <Hero>
+        <Banner
+          title="Luxurious rooms"
+          subtitle="deluxe rooms starting at GHC500"
+        >
+          <Link to="/rooms" className="btn-primary">
+            Our Rooms
+          </Link>
+        </Banner>
+      </Hero>
+
+      <Services />
+      <FeaturedRooms />
+    </>
+  );
+};
+
+export default Home;
