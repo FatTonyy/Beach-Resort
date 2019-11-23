eed54b992ae771ca9e12a6d28efc8b3c203de1c3
Fat-Tony <fat_tonyy@icloud.com>
2019-11-23 08:48:36 +0000

    fixed-img-bug

--- src/context.js
+++ src/context.js
@@ -71,7 +71,7 @@ class RoomProvider extends Component {
   formatData(items) {
     let tempItems = items.map(item => {
       let id = item.sys.id;
-      let images = item.fields.images.map(image => image.fields.file.url);
+      let images = item.fields.image.map(image => image.fields.file.url);
       let room = { ...item.fields, images, id };
       return room;
     });
